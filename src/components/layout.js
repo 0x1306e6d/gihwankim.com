f8ff9d13914ad48224ac932de53b3d6950adc0a8
Gihwan Kim <ghkim3221@gmail.com>
2020-06-21 17:42:58 +0900

    Wrap page element using Layout component

---
+++ src/components/layout.js
@@ -0,0 +1,5 @@
+import React from "react"
+
+import { Box } from "rebass"
+
+export default ({ children }) => <Box sx={{ p: 4 }}>{children}</Box>
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+import Layout from "./components/layout"
+
+export const wrapPageElement = ({ element, props }) => (
+  <Layout {...props}>{element}</Layout>
+)
