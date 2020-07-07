45cce6066f2cf842474fa100c5a7b47fb2193aa1
Gihwan Kim <ghkim3221@gmail.com>
2020-07-07 23:46:50 +0900

    Implement container component

---
+++ src/components/container.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+import { Box } from "rebass"
+
+export default ({ children }) => (
+  <Box maxWidth={1024} mx="auto">
+    {children}
+  </Box>
+)
--- src/components/layout.js
+++ src/components/layout.js
@@ -2,14 +2,15 @@ import React from "react"
 
 import { Box, Flex } from "rebass"
 
+import Container from "./container"
 import Footer from "./footer"
 import Header from "./header"
 
 export default ({ children }) => (
   <Flex flexDirection="column" sx={{ minHeight: "100vh" }}>
     <Header />
-    <Box as="main" p={4}>
-      {children}
+    <Box as="main">
+      <Container>{children}</Container>
     </Box>
     <Footer />
   </Flex>
