d24df26d8791ac9a1cd8c597014e7e79cafc0c79
Gihwan Kim <ghkim3221@gmail.com>
2020-07-14 22:33:00 +0900

    Configure Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,12 @@
 module.exports = {
-  plugins: ["gatsby-plugin-mdx", "gatsby-plugin-theme-ui"],
+  plugins: [
+    "gatsby-plugin-mdx",
+    "gatsby-plugin-theme-ui",
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "UA-138964328-3",
+      },
+    },
+  ],
 }
