ea397058d7bc7156e8b359cd97e38db226c920d7
Gihwan Kim <ghkim3221@gmail.com>
2020-08-04 01:24:44 +0900

    Add projects section

---
+++ src/components/projects.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+import { Box, Heading } from "rebass"
+
+export default () => (
+  <Box backgroundColor="white" minHeight="100vh" p={4}>
+    <Heading fontSize={[6, 7, 8]}>Projects</Heading>
+  </Box>
+)
