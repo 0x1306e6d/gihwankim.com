0572fc6f2c7698dbaad3c32e5ae3e98fb8f3f3dc
Gihwan Kim <ghkim3221@gmail.com>
2020-08-30 19:57:06 +0900

    Remove bouncing ball

--- src/components/hello.js
+++ src/components/hello.js
@@ -1,42 +1,17 @@
-import React, { useEffect, useRef, useState } from "react"
+import React from "react"
 
-import { Box, Flex, Heading } from "rebass"
+import { Flex, Heading } from "rebass"
 
-import BouncingBall from "./bouncing-ball"
-
-export default () => {
-  const ref = useRef()
-
-  const [width, setWidth] = useState(0)
-  const [height, setHeight] = useState(0)
-
-  useEffect(() => {
-    const handleResize = () => {
-      setWidth(ref.current.offsetWidth)
-      setHeight(ref.current.offsetHeight)
-    }
-
-    handleResize()
-
-    window.addEventListener("resize", handleResize)
-    return () => window.removeEventListener("resize", handleResize)
-  }, [ref])
-
-  return (
-    <Box backgroundColor="black" minHeight="100vh" ref={ref}>
-      <Flex
-        alignItems="center"
-        flexDirection="column"
-        justifyContent="center"
-        minHeight="100vh"
-      >
-        <Heading color="white" fontSize={[7, 8]} fontWeight="bold">
-          HELLO
-        </Heading>
-      </Flex>
-      <Box sx={{ position: "absolute", top: 0, height: 0 }}>
-        <BouncingBall width={width} height={height} />
-      </Box>
-    </Box>
-  )
-}
+export default () => (
+  <Flex
+    alignItems="center"
+    backgroundColor="black"
+    flexDirection="column"
+    justifyContent="center"
+    minHeight="100vh"
+  >
+    <Heading color="white" fontSize={[7, 8]} fontWeight="bold">
+      HELLO
+    </Heading>
+  </Flex>
+)
