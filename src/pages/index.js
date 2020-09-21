3ba40a2af0f5b99f0e579e6017038addfd3bec02
Gihwan Kim <ghkim3221@gmail.com>
2020-09-21 22:56:09 +0900

    Remove hello component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,9 @@
 import React from "react"
 
-import Hello from "../components/hello"
 import Projects from "../components/projects"
 
 export default () => (
   <>
-    <Hello />
     <Projects />
   </>
 )
